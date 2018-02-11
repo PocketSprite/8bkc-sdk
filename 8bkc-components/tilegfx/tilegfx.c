4c3152b1850686cc6fa2870771e809ecb93179db
Jeroen Domburg <git@j0h.nl>
2018-02-11 14:51:03 +0800

    Add beginnings of tile-based rendering engine for native games development

---
+++ 8bkc-components/tilegfx/tilegfx.c
@@ -0,0 +1,31 @@
+#include "tilegfx.h"
+#include "8bkc-hal.h"
+
+static uint16_t *fb;
+
+void tilegfx_tile_map_render(const tilegfx_map_t *tiles, int offx, int offy, const tilegfx_rect_t *dest) {
+	uint16_t *p=fb+KC_SCREEN_W*dest->y+dest->x;
+	for (int y=dest->y; y<dest->y+dest->h; y++) {
+		uint16_t *pp=p;
+		for (int x=dest->x; x<dest->x+dest->w; x++) {
+			int tilex=(offx+x);
+			int tiley=(offy+y);
+			tilex%=tiles->w*8;
+			tiley%=tiles->h*8;
+			if (tilex<0) tilex+=tiles->w*8;
+			if (tiley<0) tiley+=tiles->h*8;
+			int tile=(tilex/8)+((tiley/8)*tiles->w);
+			*pp++=tiles->gfx[tiles->tiles[tile]*64+(tiley&7)*8+(tilex&7)];
+		}
+		p+=KC_SCREEN_W;
+	}
+}
+
+void tilegfx_init() {
+	fb=malloc(KC_SCREEN_H*KC_SCREEN_W*2);
+}
+
+void tilegfx_flush() {
+	kchal_send_fb(fb);
+}
+
---
+++ 8bkc-components/tilegfx/tilegfx.h
@@ -0,0 +1,21 @@
+/*
+TileGFX: A small tile-based renderer engine for the PocketSprite.
+*/
+#pragma once
+#include <stdint.h>
+
+typedef struct {
+	int h;
+	int w;
+	const uint16_t *gfx;
+	uint16_t tiles[];
+} tilegfx_map_t;
+
+typedef struct {
+	int x, y;
+	int w, h;
+} tilegfx_rect_t;
+
+void tilegfx_tile_map_render(const tilegfx_map_t *tiles, int offx, int offy, const tilegfx_rect_t *dest);
+void tilegfx_init();
+void tilegfx_flush();
